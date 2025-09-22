29d4b697dea62d7089df1427ff7a4a042dc30910
PheaDiel <93088592+PheaDiel@users.noreply.github.com>
2025-09-23 00:15:34 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -6,20 +6,23 @@ import RootNavigator from './src/navigation';
 import { AuthProvider } from './src/context/AuthContext';
 import { InventoryProvider } from './src/context/InventoryContext';
 import { CartProvider } from './src/context/CartContext';
+import { NotificationProvider } from './src/context/NotificationContext';
 import { theme, navigationTheme } from './src/theme/theme';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <InventoryProvider>
-        <CartProvider>
-          <PaperProvider theme={theme}>
-            <NavigationContainer theme={navigationTheme}>
-              <RootNavigator />
-            </NavigationContainer>
-          </PaperProvider>
-        </CartProvider>
-      </InventoryProvider>
-    </AuthProvider>
+    <NotificationProvider>
+      <AuthProvider>
+        <InventoryProvider>
+          <CartProvider>
+            <PaperProvider theme={theme}>
+              <NavigationContainer theme={navigationTheme}>
+                <RootNavigator />
+              </NavigationContainer>
+            </PaperProvider>
+          </CartProvider>
+        </InventoryProvider>
+      </AuthProvider>
+    </NotificationProvider>
   );
 }
